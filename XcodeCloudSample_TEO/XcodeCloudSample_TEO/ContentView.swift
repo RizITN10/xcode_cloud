407081b2730263b49dd8cc740d4354ce161e17ec
Rizwan <muhammad.rizwan@teo-intl.com>
2023-12-17 18:53:52 +0500

    Initial commit with project setup

---
+++ XcodeCloudSample_TEO/XcodeCloudSample_TEO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeCloudSample_TEO
+//
+//  Created by Rizwan Mughal on 17/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Xocde Cloud!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeCloudSample_TEO/XcodeCloudSample_TEO/XcodeCloudSample_TEOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudSample_TEOApp.swift
+//  XcodeCloudSample_TEO
+//
+//  Created by Rizwan Mughal on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudSample_TEOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
